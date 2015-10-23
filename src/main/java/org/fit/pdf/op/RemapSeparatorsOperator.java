5f7596587d4618a979ef93ba6eb3b27994b5c315
Radek Burget <burgetr@fit.vutbr.cz>
2015-10-23 16:48:00 +0200

    Remove sep. pairs, let's try a different method

--- src/main/java/org/fit/pdf/op/RemapSeparatorsOperator.java
+++ src/main/java/org/fit/pdf/op/RemapSeparatorsOperator.java
@@ -8,9 +8,11 @@ package org.fit.pdf.op;
 import java.util.List;
 import java.util.Vector;
 
+import org.fit.layout.impl.AreaGrid;
 import org.fit.layout.impl.BaseOperator;
 import org.fit.layout.model.Area;
 import org.fit.layout.model.AreaTree;
+import org.fit.segm.grouping.AreaImpl;
 
 /**
  * 
@@ -66,129 +68,17 @@ public class RemapSeparatorsOperator extends BaseOperator
     @Override
     public void apply(AreaTree atree, Area root)
     {
-        List<SepPair> pairs = findSeparatorPairs(root);
-        System.out.println("Sep pairs:");
-        for (SepPair pair : pairs)
-            System.out.println("  " + pair);
-        System.out.println("Incomplete:");
-        for (SepPair pair : pairs)
-            if (!pair.isComplete())
-                System.out.println("  " + pair);
     }
     
     //==============================================================================
     
-    
-    private List<SepPair> findSeparatorPairs(Area root)
+    private void findNeigborLeft(AreaImpl area)
     {
-        Vector<SepPair> ret = new Vector<SepPair>();
-        recursivelyFindPairs(root, ret);
-        return ret;
-    }
-    
-    private void recursivelyFindPairs(Area root, List<SepPair> pairs)
-    {
-        if (root.isSeparator())
-        {
-            //try to complete an existing pair
-            final SepPair expair = findPairFor(root, pairs);
-            if (expair != null)
-                expair.addPair(root);
-            //and create a new one
-            pairs.add(new SepPair(root));
-        }
-        for (int i = 0; i < root.getChildCount(); i++)
-            recursivelyFindPairs(root.getChildArea(i), pairs);
-    }
-    
-    private SepPair findPairFor(Area sep, List<SepPair> pairs)
-    {
-        SepPair cand = null;
-        int mindist = Integer.MAX_VALUE;
-        for (SepPair pair : pairs)
-        {
-            if (!pair.isComplete() && pair.allowsPair(sep))
-            {
-                final int dist = pair.candidateDistance(sep);
-                if (dist < mindist)
-                {
-                    cand = pair;
-                    mindist = dist;
-                }
-            }
-        }
-        return cand;
-    }
-    
-    //==============================================================================
-    
-    
-    
-    private class SepPair
-    {
-        //left or top
-        public Area s1;
-        //right or bottom 
-        public Area s2;
-        
-        public SepPair(Area s1)
-        {
-            this.s1 = s1;
-            this.s2 = null;
-        }
-        
-        public SepPair(Area s1, Area s2)
-        {
-            this.s1 = s1;
-            this.s2 = s2;
-        }
-        
-        public void addPair(Area s2)
-        {
-            this.s2 = s2;
-        }
-        
-        public boolean isComplete()
-        {
-            return (s2 != null);
-        }
-        
-        public boolean allowsPair(Area cand)
-        {
-            if (s1.isHorizontalSeparator() && cand.isHorizontalSeparator())
-            {
-                //return s1.getX1() == cand.getX1() && s1.getX2() == cand.getX2();
-                return s1.getBounds().intersectsX(cand.getBounds());
-            }
-            else if (s1.isVerticalSeparator() && cand.isVerticalSeparator())
-            {
-                //return s1.getY1() == cand.getY1() && s1.getY2() == cand.getY2();
-                return s1.getBounds().intersectsY(cand.getBounds());
-            }
-            else
-                return false;
-        }
-        
-        public int candidateDistance(Area cand)
-        {
-            if (s1.isHorizontalSeparator() && cand.isHorizontalSeparator())
-            {
-                return cand.getY1() - s1.getY2();
-            }
-            else if (s1.isVerticalSeparator() && cand.isVerticalSeparator())
-            {
-                return cand.getX1() - s1.getX2();
-            }
-            else
-                return -1;
-        }
-        
-        @Override
-        public String toString()
-        {
-            return "[" + s1 + " : " + s2 + "]";
-        }
+        final AreaGrid grid = area.getGrid();
+        int y = area.getGridPosition().midY();
         
     }
+        
+    
     
 }
